26b93fc22b8d3d05fff3709e0f3f2ae348e3be40
Amir <rahavamir1@gmail.com>
2024-08-01 20:06:48 +0300

    Add MainView page with data fetching from API

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,15 +1,52 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import axios from 'axios';
 import { Grid } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
+const url = 'https://swapi.dev/api/people/';
+
 const MainView = () => {
+  const [listItems, setListItems] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchData = useCallback(async () => {
+    if (isLoading) return;
+    setIsLoading(true);
+
+    try {
+      const response = await axios.get(`${url}?page=${page}`);
+  
+      setListItems(prevData => [...prevData, ...response?.data?.results]);
+      setPage(prevPage => prevPage + 1);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  }, [page, isLoading]);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(`${url}?page=${page}`);
+        setListItems(prevData => [...prevData, ...response?.data?.results]);
+      } catch (error) {
+        console.log(error);
+      }
+      setIsLoading(false);
+    };
+
+    getData();
+  }, []);
+
   return (
-    <Grid container item xs={8}>
-      <Grid container pt={5}>
-        <Outlet />
-      </Grid>
-    </Grid>
-  )
+		<Grid container item xs={6}>
+			<Grid container sx={{ height: "100%" }}>
+				<Outlet context={{ listItems, fetchData }} />
+			</Grid>
+		</Grid>
+	)
 }
 
 export default MainView;
