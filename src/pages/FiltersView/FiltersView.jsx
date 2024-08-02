a0704c382e75d64ae9d527ec3af164d6322fed8c
Amir <rahavamir1@gmail.com>
2024-08-03 01:27:04 +0300

    Add FiltersView page with filter functionality
    - Implemented FiltersView component for filtering list items by name, age, and gender.
    - Added localStorage integration for persisting filters.
    - Included count display for filtered results.

--- src/pages/FiltersView/FiltersView.jsx
+++ src/pages/FiltersView/FiltersView.jsx
@@ -1,9 +1,119 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { Grid, Card, CardContent, TextField, Typography, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { useOutletContext } from 'react-router-dom';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+
+const genderOptions = ['Male', 'Female', 'Other'];
+const MAX_CHARS = 35;
 
 const FiltersView = () => {
-  return (
-    <div>FiltersView</div>
-  )
+  const { updateFilters , listItems, setFilteredList } = useOutletContext();
+  const localStorage = useLocalStorage();
+	const [count, setCount] = useState(0);
+	const [formData, setFormData] = useState(localStorage.getItem("filters") || {
+		name: "",
+		age: "",
+		gender: "",
+	});
+
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+    const newFilters = { ...formData, [name]: value };
+		setFormData(newFilters);
+	};
+
+  const applyFilters = useCallback((filters) => {
+		const hasEmptyFilters = Object.values(filters).every(value => value === "");
+    const updatedListItems = listItems?.filter(item => {
+      return (
+        (!filters.name || item?.name?.toLowerCase().includes(filters?.name.toLowerCase())) &&
+        (!filters.age || item?.birth_year?.includes(filters?.age)) &&
+        (!filters.gender || item?.gender?.toLowerCase() === filters?.gender.toLowerCase())
+      );
+    })
+		
+    setCount(!hasEmptyFilters ? updatedListItems?.length : 0);
+    setFilteredList(updatedListItems);
+  }, [listItems]);
+
+  useEffect(() => {
+    localStorage.setItem("filters", formData);
+    applyFilters(formData);
+    updateFilters(formData);
+  }, [formData, updateFilters, applyFilters]);
+
+	return (
+		<Grid container flexDirection={"column"} justifyContent={"space-around"}>
+			<Card
+				sx={{
+					maxHeight: "100%",
+				}}
+			>
+				<CardContent sx={{ height: "100%" }}>
+					<Grid container wrap="nowrap" height={"100%"}>
+						{/* Left Panel */}
+						<Grid container item flexDirection={"column"} rowSpacing={4}>
+							<Grid item>
+								<Typography variant="h6">Search Star Wars Figures</Typography>
+							</Grid>
+
+							<Grid item>
+								<TextField
+									label="Name"
+									name="name"
+									type="search"
+									variant="outlined"
+									value={formData?.name}
+									onChange={handleInputChange}
+									fullWidth
+								/>
+							</Grid>
+
+							<Grid item>
+								<TextField
+									label="Age"
+									name="age"
+									type="search"
+									variant="outlined"
+									value={formData?.age}
+									onChange={handleInputChange}
+									fullWidth
+								/>
+							</Grid>
+
+							<Grid item>
+								<FormControl fullWidth>
+									<InputLabel>Gender</InputLabel>
+									<Select
+										name="gender"
+										value={formData?.gender}
+										label="Gender"
+										onChange={handleInputChange}
+									>
+										{genderOptions.map((gender) => (
+											<MenuItem key={gender} value={gender}>
+												{gender}
+											</MenuItem>
+										))}
+									</Select>
+								</FormControl>
+							</Grid>
+						</Grid>
+
+						{/* Right Panel */}
+						<Grid container item flexDirection={"column"} justifyContent={"flex-start"} alignContent={"space-around"}>
+							<Grid item>
+								<Typography variant="h6">Count:</Typography>
+							</Grid>
+              <Grid item>
+								<Typography variant="h3">{count}</Typography>
+              </Grid>
+						</Grid>
+					</Grid>
+				</CardContent>
+			</Card>
+		</Grid>
+	);
 }
 
 export default FiltersView;
