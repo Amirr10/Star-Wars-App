ee31436dcad0a5158526df6d3a69dcb7475153e8
Amir <rahavamir1@gmail.com>
2024-08-03 03:28:45 +0300

    Added styling for Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Box, Grid } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Box, Grid, Icon } from '@mui/material';
+import StarsIcon from '@mui/icons-material/Stars';
 import { Link } from 'react-router-dom';
+import { styles } from './styles';
 
 const pages = [
   { text: "Catalogue", link: "/catalogue" },
@@ -9,28 +11,31 @@ const pages = [
 
 const Navbar = () => {
   return (
-		<AppBar position="sticky" sx={{ bgcolor: "#496cae" }}>
+		<AppBar position="sticky" sx={styles.navbar}>
 			<Toolbar>
-				<Grid container alignItems={"baseline"}>
+				<Grid container alignItems={"baseline"} pl={7}>
 					<Grid item>
 						<Button
 							component={Link}
 							to={"/"}
-							sx={{ my: 2, color: "white", textTransform: "none" }}
+							sx={styles.linkButton}
 						>
-							<Typography variant="h6" pl={7} sx={{ flexGrow: 1 }}>
+							<Icon>
+								<StarsIcon />
+							</Icon>
+							<Typography variant="h6" sx={{ flexGrow: 1 }}>
 								Star Wars App
 							</Typography>
 						</Button>
 					</Grid>
 					<Grid item>
-						<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+						<Box sx={styles.boxButtons}>
 							{pages.map((page, idx) => (
 								<Button
 									key={idx}
 									component={Link}
 									to={page.link}
-									sx={{ my: 2, color: "white", textTransform: "none" }}
+									sx={styles.linkButton}
 								>
 									<Typography variant="subtitle2" pl={2}>
 										{page.text}
---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,14 @@
+export const styles = {
+	navbar: {
+    backgroundColor: "#496cae"
+  },
+  linkButton: {
+    my: 2,
+    color: "white",
+    textTransform: "none" 
+  },
+  boxButtons: {
+    display: { xs: "none", md: "flex" },
+    flexGrow: 1
+  }
+};
